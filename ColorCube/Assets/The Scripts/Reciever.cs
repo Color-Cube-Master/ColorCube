08210595887f04ea63e652974cf9d5654aaea347
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-02-05 21:54:08 +0100

    Shaders/Movement/Bounce animation/Player models
    1-An example of shader created with the shader graph :Shaders+Materials directory.
    2-Movement and bouncing of the Sphere : PlayerMovement scene.
    -Player models. (1 example for now):Player Models directory.

---
+++ ColorCube/Assets/The Scripts/Reciever.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reciever : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    void OnEnable() {
+        Sender.ButtonPressedInfo += ButtonPressedListener;
+    }
+    void OnDisable() {
+        Sender.ButtonPressedInfo -= ButtonPressedListener;
+    }
+    void ButtonPressedListener()
+    {
+        Vector3 move = new Vector3();
+        if (Input.GetKey(KeyCode.UpArrow))
+            move.z += speed;
+        if (Input.GetKey(KeyCode.DownArrow))
+            move.z -= speed;
+        if (Input.GetKey(KeyCode.RightArrow))
+            move.x += speed;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            move.x -= speed;
+        
+
+        transform.position += move;
+
+    }
+
+
+}
---
+++ ColorCube/Assets/The Scripts/Sender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sender : MonoBehaviour
+{
+    public delegate void ButtonPressed();
+    public static event ButtonPressed ButtonPressedInfo;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Vertical") || Input.GetButtonDown("Horizontal"))
+        {
+            ExEvent();
+        }
+    }
+    void ExEvent()
+    {
+        
+            if (ButtonPressedInfo!= null)
+            {
+                ButtonPressedInfo();
+            }
+       
+
+    }
+}
