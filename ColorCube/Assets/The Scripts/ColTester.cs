ca80af9160dbc8569bdd7f5d66c08f5c0568c2d8
dota2noob322 <60358455+dota2noob322@users.noreply.github.com>
2020-02-11 20:05:23 +0100

    Collision + cube color change
    worked in the Playermovement scene
    added a script called ColTester
    added 2 materials called GreenTester and RedTester
    made  the cube(1) green by default
    gave the sphere a tag " Player"
    made the cube change color on collision red to green

---
+++ ColorCube/Assets/The Scripts/ColTester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColTester : MonoBehaviour
+{
+    public Material Green;
+    public Material Red;
+    int Counter;
+    Renderer rend;
+    void Start ()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if ((col.gameObject.tag == "Player") && (Counter%2==0))
+        {
+            rend.material = Red;
+            Counter++;
+        }
+        else
+        {
+            rend.material = Green;
+            Counter++;
+        }
+        
+    }
+}
