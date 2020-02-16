b1d017aaeceea83be002c723c1aa1ab43e1b519c
dota2noob322 <60358455+dota2noob322@users.noreply.github.com>
2020-02-16 14:22:52 +0100

    Cube destruction
    worked on Coltester script and cube(1) and cube(2)
    added destroy function that destroys the cube after a random number of hits
    every cube has his own random number of hits to get destroyed
    added cube(2) to the experience with the green and red colors

--- ColorCube/Assets/The Scripts/ColTester.cs
+++ ColorCube/Assets/The Scripts/ColTester.cs
@@ -2,28 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ColTester : MonoBehaviour
 {
     public Material Green;
     public Material Red;
     int Counter;
+    int HitsMAX;
+
+
     Renderer rend;
     void Start ()
     {
         rend = GetComponent<Renderer>();
+        HitsMAX = UnityEngine.Random.Range(1,9);
+        //show the number of hits needed for the cube to get destroyed
+        //Debug.Log(HitsMAX);
     }
+    // This function is for destroying the cube 
+    void Destroy()
+    {
+        if (Counter == HitsMAX-1)
+        { Destroy(gameObject); }
+    }
+    // this function is for the collision and the color changing 
     void OnCollisionEnter(Collision col)
     {
-        if ((col.gameObject.tag == "Player") && (Counter%2==0))
+        if ((col.gameObject.tag == "Player") && (Counter % 2 == 0))
         {
+
+            Destroy();
+
+
+
             rend.material = Red;
             Counter++;
+
         }
         else
         {
-            rend.material = Green;
-            Counter++;
+            Destroy();
+
+            {
+                rend.material = Green;
+                Counter++;
+            }
+
+
         }
-        
     }
 }
