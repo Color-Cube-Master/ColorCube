36be2bab29e32a22a5ea9bd2155503a4eaff5fac
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-03-12 17:17:12 +0100

    FINAL LEVEL EDITOR
    This new level editor features:
    *Choosing how many levels you want to create in the game.
    *Choosing the size of each level.
    *In each level you can choose the prefab you want to use to make the level.
    *In each level you can choose which boxes will be non existent in order to customize the level.
    -> To test the level go to
    (Assets>Scenes>New level editor).

--- ColorCube/Assets/The Scripts/LevelSpawner.cs
+++ ColorCube/Assets/The Scripts/LevelSpawner.cs
@@ -7,15 +7,13 @@ public class LevelSpawner : MonoBehaviour
 {
     //Table of Maps(Matrixs)
     public Map[] level;
-    public float spawnSpeed = 0;
     public static int k;
     public static int r;
-    //public static int eh;
 
     void Start()
     {
         
-        StartCoroutine(SpawnLevel());
+        SpawnLevel();
         
     }
     void Update()
@@ -23,63 +21,37 @@ public class LevelSpawner : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow))
         {
             SceneManager.LoadScene("New level editor");
-            StartCoroutine(SpawnLevel());
-
-
-
         }
 
     }
 
 
 
-    IEnumerator SpawnLevel()
+    void SpawnLevel()
     {
 
 
 
         Map grid = level[k];
-
-
-        //int cordx = grid.xprefabs[r].cordx;
-        //int cordY = grid.xprefabs[r].cordY;
         
         int xlength= grid.xprefabs.Length;
-       /* int cordx1 = grid.xprefabs[1].cordx;
-        int cordY1 = grid.xprefabs[1].cordY;*/
-
-
-
 
 
 
         for (int i = 0; i < grid.x; i++)
         {
-            yield return new WaitForSeconds(spawnSpeed);
+            
             for (int j = 0; j < grid.Y; j++)
             {
-                yield return new WaitForSeconds(spawnSpeed);
+               
                 /*Instantiate(Resources.Load("Cube", typeof(GameObject))) as GameObject;*/
                  GameObject sandouk = Instantiate(grid.prefab) as GameObject;
                 sandouk.transform.position = new Vector3(j * 1f, i * -1f, 0);
 
                 {
-                   /* if (sandouk.transform.position.x == cordx & sandouk.transform.position.y == cordY)
-                    {
-                        Debug.Log("this is from matrice number  " + k + " sandouk with the cord x =  " + cordx + " et cord y = " + cordY);
-                        Destroy(sandouk);
-                        
-                    }*/
-                    
+                 
                     DespawnSandouk(sandouk , grid, ref r, xlength, k);
                     
-
-                    /*if (sandouk.transform.position.x == cordx1 & sandouk.transform.position.y == cordY1)
-                    {
-                        Debug.Log("this is from matrice number  " + k + " sandouk with the cord x =  " + cordx1 + " et cord y = " + cordY1);
-                        Destroy(sandouk);
-                    }*/
-
                 }
             }
 
@@ -87,16 +59,11 @@ public class LevelSpawner : MonoBehaviour
         if (k+1 < level.Length)
             k++;
         else
-        { k = 0; }
-       /* if (eh < xlength)
-            r++;
-        else
-        { eh = 0; }*/
-       
-       
-        
+        { k = 0; }    
    
     }
+
+
      void DespawnSandouk(GameObject sndk , Map grd , ref int counter , int l, int elk)
         {
         
@@ -108,35 +75,14 @@ public class LevelSpawner : MonoBehaviour
                 Destroy(sndk);
                 Debug.Log("Counter=" + counter);
                 Debug.Log("r=" + r);
-                // <l-1 caus counter starts from 0 and l starts from 1 
+                
+                // <l-1 because counter starts from 0 and l starts from 1 
                 if (counter < l-1)
                 counter++;
                 else
                     counter = 0;
 
-
-
             }
         
-    }  
+        }  
 }
-                
-            
-
-        
-
-       
-    
-
-
-
-         
-
-
- 
-
-
-
-
-
-
