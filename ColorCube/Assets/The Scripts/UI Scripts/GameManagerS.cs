479147c3128be93c9ad9ac3a06d4165029e6ff8d
dota2noob322 <60358455+dota2noob322@users.noreply.github.com>
2020-03-14 19:10:19 +0100

    New scens + audio manager + changes
    Created 2 new scenes Intro and timer
    the timer scene is empty will work on it later on
    The intro scene
    Has a canvas with image in it
    the image will change between 3 2 1 GO  with an animation
    every time something appers there is audio for it meaining when number 3 appears there is
    audio of number 3 that can be heard
    all audio are added with the audio manager that can be found in the prefabs folder
    made it a global item caus we're gonna use it in all the scenes from now on if we want to add any sound
    
    How does audio manager work ?
    simple you give the size and it gives you where to put the name of the audio the source of the audio and the volume
    
    added gamemanager script
    added audiomanager script
    added sound script
    added setcountdown script
    
    all those scripts are inside scripts folder in a forler called UI scripts
    
    Audio manager will be very helpfun when adding any sound in any scene
    to add the sound just type
    FindObjectOfType<AudioManager>().Play("INSERT NAME HERE");
    in any function in any script and it will be played

---
+++ ColorCube/Assets/The Scripts/UI Scripts/GameManagerS.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+
+public class GameManagerS : MonoBehaviour
+{
+    [HideInInspector]
+    public int CountDownState = 0;
+    
+
+    void Update()
+    {
+
+        InvokeRepeating("PlaySound", 0.7f, 1f);
+    }
+    void PlaySound()
+    {
+        
+        if(CountDownState==0)
+        {
+            FindObjectOfType<AudioManager>().Play("Three");
+            
+
+        }
+        if (CountDownState == 1)
+        {
+            FindObjectOfType<AudioManager>().Play("Two");
+            
+
+        }
+        if (CountDownState == 2)
+        {
+            FindObjectOfType<AudioManager>().Play("One");
+            
+
+        }
+        if (CountDownState == 3)
+        {
+            FindObjectOfType<AudioManager>().Play("Let's go");
+
+
+        }
+        if (CountDownState == 4 )
+        {
+            SceneManager.LoadScene("New level editor");
+        }
+    }
+}
---
+++ ColorCube/Assets/The Scripts/UI Scripts/SetCountdown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+public class SetCountdown : MonoBehaviour
+{
+    private GameManagerS GMS;
+    public void ChangeCountdown()
+    {
+        
+        GMS = GameObject.Find("GameManager").GetComponent<GameManagerS>();
+        GMS.CountDownState++;  
+
+
+    }
+}
