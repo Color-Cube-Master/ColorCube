c6773d12f01920a37ff6b5c4306a104f538c96c9
dota2noob322 <60358455+dota2noob322@users.noreply.github.com>
2020-03-24 12:48:03 +0100

    Movement fix

---
+++ ColorCube/Assets/The Scripts/Bounce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Bounceit();
+
+    }
+    void Bounceit()
+    {
+        y = transform.position.y;
+        if (y <= 0.211f)
+            LeanTween.moveY(gameObject, 1.861f, 0.45f);
+        if (y == 1.861f)
+            LeanTween.moveY(gameObject, 0.210f, 0.45f);
+
+    }
+}
