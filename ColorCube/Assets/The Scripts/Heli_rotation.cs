cd4e7c406b20a31f3755767e4af5fa5a04117325
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-04-21 15:12:10 +0100

    Final designs (laser)
    #Designed the 'Laser_copter' model from scratch using 3ds max
    
    #Textured the model using different materials in 3ds max
    
    #Created the 'Heli_float' animation
    
    #Programmed the laser copter to fly by coding the 'Heli_rotation' script.
    (All the final designs  can be viewed in the props scene).

---
+++ ColorCube/Assets/The Scripts/Heli_rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heli_rotation : MonoBehaviour
+{ public float rotation_speed ; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+{
+    transform.Rotate (0,0,-rotation_speed*Time.deltaTime); //rotates (rotation_speed) degrees per second around z axis
+}
+}
