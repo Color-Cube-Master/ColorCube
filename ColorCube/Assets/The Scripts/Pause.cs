d52b52c589b7ad7c0a0426570c460b51bbed3400
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-05-05 22:15:37 +0100

    Beta v0.1
    #Created two beta levels
    
    #Updated the 'ColTester2' code making the player able to move from level 1 to level 2 once he finishes the first level (Looped until further update)
    
    #Created the 'PlayerSpawner'
    
    #Coded the 'Pause' script
    
    #Programmed the pause icon to change states on click
    
    #Programmed the pause button to change states on click
    
    #Programmed the game to pause when the player clicks on the pause button.
    (Check 'Beta' scene for more).

---
+++ ColorCube/Assets/The Scripts/Pause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+     bool IsPaused = false ; 
+     public Sprite Playing;
+     public Sprite Paused ;
+     public Image PauseI;
+     public Sprite NotPressed;
+     public Sprite Pressed ;
+     public Button PauseB;
+
+    public void PauseGame () {
+        //Time svale controls the speed of the game
+        if (IsPaused)
+        {
+         Time.timeScale = 1;
+         IsPaused = false;
+        }
+
+        else
+        {
+        Time.timeScale=0;
+        IsPaused = true;
+        }
+
+    }
+    //Change the button sprite 
+    public void ChangeButton(){
+
+     if (PauseB.image.sprite == NotPressed)
+
+         PauseB.image.sprite = Pressed;
+
+     else 
+         PauseB.image.sprite = NotPressed;
+    }
+
+    //Change the pause icon
+    public void ChangeIcon(){
+
+     if (PauseI.sprite == Playing)
+
+         PauseI.sprite = Paused;
+
+     else 
+         PauseI.sprite = Playing;
+    }
+
+}
