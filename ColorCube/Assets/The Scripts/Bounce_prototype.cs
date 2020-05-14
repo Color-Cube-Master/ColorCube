c8950cbb7ab79f62ca36ee753ffba00f51d4aa15
dota2noob322 <60358455+dota2noob322@users.noreply.github.com>
2020-05-14 11:56:52 +0100

    Added slow motion and timer scene
    2 new scenes timer and slow motion
    made a change to the bounce prototype script to work accordingly if it is in slow mode it goes slow else it goes at it's normal speed

--- ColorCube/Assets/The Scripts/Bounce_prototype.cs
+++ ColorCube/Assets/The Scripts/Bounce_prototype.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;  
 public class Bounce_prototype : MonoBehaviour
 {
     float y;
     public Reciever reciever;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+     Scene m_Scene;
+  string sceneName;
+  private void Start()
+     {
+         m_Scene = SceneManager.GetActiveScene();
+         sceneName = m_Scene.name;
+     }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -20,7 +24,23 @@ public class Bounce_prototype : MonoBehaviour
     }
     void Bounceit()
     {
-      y = transform.position.y;
+       if (sceneName == "SlowMode")
+      {y = transform.position.y;
+        if (y <= 0.801f)
+        {
+         
+            LeanTween.moveY(gameObject, 2.2f, 0.7f);
+            
+        }
+            
+        if (y == 2.2f)
+        {
+           
+            LeanTween.moveY(gameObject, 0.800f, 0.8f);
+        }
+      }  
+      else 
+       {y = transform.position.y;
         if (y <= 0.801f)
         {
          
@@ -33,6 +53,6 @@ public class Bounce_prototype : MonoBehaviour
            
             LeanTween.moveY(gameObject, 0.800f, 0.3f);
         }
-           
+       }  
     }
 }
