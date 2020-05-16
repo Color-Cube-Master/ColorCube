5d50b39e5adca9f6023c0a6167c2303fd73b5d16
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-05-16 23:28:37 +0100

    Upgrade
    #Upgraded some files.

--- ColorCube/Assets/The Scripts/ColTester2.cs
+++ ColorCube/Assets/The Scripts/ColTester2.cs
@@ -93,7 +93,7 @@ public class ColTester2 : MonoBehaviour
                           
                           
                  
-                    //SceneManager.LoadScene("Beta");
+                    
                     
                     
 
