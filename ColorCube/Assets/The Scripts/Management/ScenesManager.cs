32fb8dd61bc37dd65830f72182e336d203f9ed24
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-05-27 22:17:06 +0100

    SceneManager+
    #Fixed the 'You Win' scene by adding it's correspondent sound
    
    #Created a new 'SceneManager' to manage all the transitions between the scenes
    
    #Coded the 'ScenesManager' script to make the button dynamic and load new scenes.
    (Back button works exclusively for te 'Timer' challenge until further updates).

---
+++ ColorCube/Assets/The Scripts/Management/ScenesManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+ 
+ public Button Back;
+ public Sprite NotPressed;
+ public Sprite Pressed ;
+ static int sceneIndex;
+ 
+ 
+    /*void Awake() {
+
+    			DontDestroyOnLoad(gameObject);
+                
+
+
+    }
+
+     void Start () {
+     sceneIndex = SceneManager.GetActiveScene().buildIndex;	
+	}*/
+
+    public void ChangeButton(){
+
+     if (Back.image.sprite == NotPressed)
+
+         Back.image.sprite = Pressed;
+
+     else
+
+         Back.image.sprite = NotPressed;
+     
+    }
+
+
+    public void GoBack () {
+        //Time svale controls the speed of the game
+        if (Back.image.sprite = Pressed)
+        {
+         SceneManager.LoadScene("Timer");
+        //SceneManager.LoadScene (sceneIndex - 1);
+        }
+    }
+
+       
+}
