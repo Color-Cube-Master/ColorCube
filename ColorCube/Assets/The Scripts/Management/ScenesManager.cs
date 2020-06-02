5cd2b97fb13160104e90a51d4c7ce29071bff981
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-06-02 03:46:54 +0100

    Full game scenes management
    #Coded all the 5 menu buttons in the 'ScenesManager' to be dynamic featuring an animation and a redirect to the correct scene depending on the player's choice
    
    #Coded 6 functions in the 'ScenesManager' to animate the back buttons in all scenes and program them to redirect the player back to the desired scene
    
    #Coded 3 functions from which one starts the game and 2 others to redirect the players to one of  the shops after accessing the 'Shop' scene
    
    #Imbedded the scene manager into 'You Win' scene and in 'You Lose'  scenes to fix animation errors
    
    #Added the button component to all working buttons and created a press effect (highlight on press or a sprite change)
    
    #Created a new menu button and designed it's icon in normal mode, 'You Lose' scene and in 'You Win' scene so the player can always go back to the 'Menu' scene
    
    #Changed both shops user interfaces for all models and themes
    
    (From now on the game always starts from the 'Menu' scene.).

--- ColorCube/Assets/The Scripts/Management/ScenesManager.cs
+++ ColorCube/Assets/The Scripts/Management/ScenesManager.cs
@@ -8,8 +8,31 @@ public class ScenesManager : MonoBehaviour
 {
  public Button Retry;
  public Button Next;
+ public Button Settings;
+ public Button Shop;
+ public Button Leaderboard;
+ public Button Challenges;
+ 
+
+ public Button Play;
+
+
  public Sprite NotPressed;
  public Sprite Pressed ;
+ public Sprite NotPressedSettings;
+ public Sprite PressedSettings ;
+ public Sprite NotPressedShop;
+ public Sprite PressedShop ;
+ public Sprite NotPressedLeaderboard_Ch;
+ public Sprite PressedLeaderboard_Ch;
+ public Sprite NotPressedMenu;
+ public Sprite PressedMenu;
+
+
+
+
+   
+ 
  
  //Old method
  /*Application.LoadLevel(ColTester2.sceneIndex);
@@ -22,12 +45,11 @@ public class ScenesManager : MonoBehaviour
     			DontDestroyOnLoad(gameObject);
 
                  }
-
-    
-
+   
+//Changing the buttons sprites on press-------------------------------------------------------------------------------------------------------
     public void ChangeButtonNext(){
 
-     if (Next.image.sprite == NotPressed)
+     if (Next.image.sprite == NotPressed )
 
          Next.image.sprite = Pressed;
 
@@ -49,7 +71,210 @@ public class ScenesManager : MonoBehaviour
      
     }
 
+    //_Switching scenes on press-----------------------------------------------------------------------------------------------------------
+public void ChangeButtonSettings(){
 
+     if (Settings.image.sprite == NotPressedSettings)
+{
+         Settings.image.sprite = PressedSettings;
+         SceneManager.LoadScene("Settings");
+}
+
+     else
+
+         Settings.image.sprite = NotPressedSettings;
+     
+    }
+
+    public void ChangeButtonShop(){
+
+     if (Shop.image.sprite == NotPressedShop)
+{
+         Shop.image.sprite = PressedShop;
+         SceneManager.LoadScene("Shop");
+}
+
+     else
+
+         Shop.image.sprite = NotPressedShop;
+     
+    }
+
+    public void ChangeButtonLeaderboard(){
+
+     if (Leaderboard.image.sprite == NotPressedLeaderboard_Ch)
+{
+         Leaderboard.image.sprite = PressedLeaderboard_Ch;
+         SceneManager.LoadScene("Leaderboards");
+}
+
+     else
+
+         Leaderboard.image.sprite = NotPressedLeaderboard_Ch;
+     
+    }
+
+    public void ChangeButtonChallenges(){
+
+     if (Challenges.image.sprite == NotPressedLeaderboard_Ch)
+{
+         Challenges.image.sprite = PressedLeaderboard_Ch;
+         SceneManager.LoadScene("Challenges");
+
+} 
+     else
+
+         Challenges.image.sprite = NotPressedLeaderboard_Ch;
+     
+    }
+
+    public void ChangeButtonMenu(){
+
+       GameObject Menu2 = GameObject.FindWithTag("Menu");
+       Button Menu = Menu2.GetComponent<Button>();
+
+     /*if (Menu.image.sprite == NotPressedMenu)
+    {
+         Menu.image.sprite = PressedMenu;*/
+         SceneManager.LoadScene("Menu");
+         
+
+/*} 
+     else
+
+         Menu.image.sprite = NotPressedMenu;*/
+     
+    }
+
+//Backs---------------------------------------------------------------------------------------------------------------------------------------
+    public void ChangeButtonChallengesBack(){
+
+       GameObject ChallengesBack2 = GameObject.FindWithTag("BackCh");
+       Button ChallengesBack = ChallengesBack2.GetComponent<Button>();
+
+     if (ChallengesBack.image.sprite == NotPressed)
+{
+         ChallengesBack.image.sprite = Pressed;
+         SceneManager.LoadScene("Menu");
+         
+
+} 
+     else
+
+         ChallengesBack.image.sprite = NotPressed;
+     
+    }
+
+public void ChangeButtonSettingsBack(){
+
+       GameObject SettingsBack2 = GameObject.FindWithTag("BackSe");
+       Button SettingsBack = SettingsBack2.GetComponent<Button>();
+
+     if (SettingsBack.image.sprite == NotPressed)
+{
+         SettingsBack.image.sprite = Pressed;
+         SceneManager.LoadScene("Menu");
+         
+
+} 
+     else
+
+         SettingsBack.image.sprite = NotPressed;
+     
+    }
+
+    public void ChangeButtonShopBack(){
+
+       GameObject ShopBack2 = GameObject.FindWithTag("BackSh");
+       Button ShopBack = ShopBack2.GetComponent<Button>();
+
+     if (ShopBack.image.sprite == NotPressed)
+{
+         ShopBack.image.sprite = Pressed;
+         SceneManager.LoadScene("Menu");
+         
+
+} 
+     else
+
+         ShopBack.image.sprite = NotPressed;
+     
+    }
+
+     public void ChangeButtonLeaderBoardBack(){
+
+       GameObject LeaderBoardBack2 = GameObject.FindWithTag("BackLe");
+       Button LeaderBoardBack = LeaderBoardBack2.GetComponent<Button>();
+
+     if (LeaderBoardBack.image.sprite == NotPressed)
+{
+         LeaderBoardBack.image.sprite = Pressed;
+         SceneManager.LoadScene("Menu");
+         
+
+} 
+     else
+
+         LeaderBoardBack.image.sprite = NotPressed;
+     
+    }
+
+     public void ChangeButtonThemeBack(){
+
+       GameObject ThemeBack2 = GameObject.FindWithTag("BackTh");
+       Button ThemeBack = ThemeBack2.GetComponent<Button>();
+
+     if (ThemeBack.image.sprite == NotPressed)
+{
+         ThemeBack.image.sprite = Pressed;
+         SceneManager.LoadScene("Shop");
+         
+
+} 
+     else
+
+         ThemeBack.image.sprite = NotPressed;
+     
+    }
+
+    public void ChangeButtonCharactersBack(){
+
+       GameObject CharactersBack2 = GameObject.FindWithTag("BackChar");
+       Button CharactersBack = CharactersBack2.GetComponent<Button>();
+
+     if (CharactersBack.image.sprite == NotPressed)
+{
+         CharactersBack.image.sprite = Pressed;
+         SceneManager.LoadScene("Shop");
+         
+
+} 
+     else
+
+         CharactersBack.image.sprite = NotPressed;
+     
+    }
+
+    //2shops--------------------------------------------------------------------------------------------------------------------------------
+ public void ChangeButtonThemes(){
+
+       GameObject Themes2 = GameObject.FindWithTag("Themes");
+       Button Themes = Themes2.GetComponent<Button>();
+
+         SceneManager.LoadScene("ThemeShopScene");
+        
+    }
+
+    public void ChangeButtonCharacters(){
+
+       GameObject Characters2 = GameObject.FindWithTag("Characters");
+       Button Characters = Characters2.GetComponent<Button>();
+
+         SceneManager.LoadScene("ModelShopScene");
+        
+    }
+
+//Action on press---------------------------------------------------------------------------------------------------------------------------
     public void GoNext (string sceneName) {
        
         if (Next.image.sprite = Pressed)
@@ -73,5 +298,9 @@ public class ScenesManager : MonoBehaviour
              SceneManager.LoadScene(LevelSpawner.sceneName);        
     }
 
+//Plays--------------------------------------------------------------------------------------------------------------------------------------
+public void StartGame() {
+    SceneManager.LoadScene("Beta");
+}
       
 }
