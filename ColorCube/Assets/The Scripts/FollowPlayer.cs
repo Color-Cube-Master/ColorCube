321432421195c223aa2042d5fe95934a7c0f9458
T-Racem <60358956+T-Racem@users.noreply.github.com>
2020-06-04 16:43:50 +0100

    Camera
    #Created the 'FollowPlayer' script to make the camera follow the player.
    
    #Added and configured the script to all playable scenes.

---
+++ ColorCube/Assets/The Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{ 
+         
+	public float smoothSpeed = 2f;
+	public Vector3 offset;
+
+	void FixedUpdate ()
+	{  GameObject Player = GameObject.FindWithTag("Player");
+           Transform PlayerT = Player.transform;
+		Vector3 desiredPosition = PlayerT.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+		transform.position = smoothedPosition;
+
+		transform.LookAt(PlayerT);
+	}
+}
