4d0afe951de4891283a8a462bdf5e13707267cbf
dota2noob322 <60358455+dota2noob322@users.noreply.github.com>
2020-06-08 23:59:38 +0100

    fix
    fixed the themes and characters not working in challenges

--- ColorCube/Assets/The Scripts/PlayerChange.cs
+++ ColorCube/Assets/The Scripts/PlayerChange.cs
@@ -90,54 +90,7 @@ public class PlayerChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Model.Name=="1-Yellow ball") 
-      LevelSpawner.Playername= YellowBall;
-       else if (Model.Name=="2-DustyBall") 
-      LevelSpawner.Playername= dustyball;
-       else if (Model.Name=="3-BeachBall") 
-      LevelSpawner.Playername= Beachball;
-       else if (Model.Name=="4-BasketBall") 
-      LevelSpawner.Playername=  Basketball;
-       else if (Model.Name=="5-Golf_ball") 
-      LevelSpawner.Playername= golfball;
-       else if (Model.Name=="6-TennisBall") 
-      LevelSpawner.Playername= tennisball;
-       else if (Model.Name=="7-Ornament") 
-      LevelSpawner.Playername= ornament;
-       else if (Model.Name=="8-Baseball") 
-      LevelSpawner.Playername= Baseball;
-       else if (Model.Name=="9-Soccer_ball") 
-      LevelSpawner.Playername= soccerball;
-       else if (Model.Name=="10-TooBlueBall") 
-      LevelSpawner.Playername= tooblueball;
-       else if (Model.Name=="11-ChaosBall") 
-      LevelSpawner.Playername= chaosball;
-       else if (Model.Name=="12-CricketBall") 
-      LevelSpawner.Playername= Cricketball;
-      else if (Model.Name=="13-LoveEmoji") 
-      LevelSpawner.Playername= loveEmo;
-       else if (Model.Name=="14-DiscoBall") 
-      LevelSpawner.Playername= discoball;
-      else if (Model.Name=="15-Ball droid") 
-      LevelSpawner.Playername= Droid;
-       else if (Model.Name=="16-8Ball") 
-      LevelSpawner.Playername= eightball;
-       else if (Model.Name=="17-Eyeball") 
-      LevelSpawner.Playername= eye; 
-       else if (Model.Name=="18-AtomesBall") 
-      LevelSpawner.Playername= Buckyball;
-       else if (Model.Name=="19-PokemonBall") 
-      LevelSpawner.Playername=pokemonball;
-      else  if (Model.Name=="20-Lowpoly_earth") 
-      LevelSpawner.Playername= earth;
-       else if (Model.Name=="21-SpikedBall") 
-      LevelSpawner.Playername= spikeball;
-       else if (Model.Name=="22-Champions_league ball") 
-      LevelSpawner.Playername= CLBall;
-       else if (Model.Name=="23-Robot sphere") 
-      LevelSpawner.Playername= robot;
-      else
-      LevelSpawner.Playername= FirstBall; 
+        
       
     }
 }
